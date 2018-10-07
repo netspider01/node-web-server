7318a4ee36b5a92225ad06c12631deb38ac13df1
Jun Feng <jun_feng@apple.com>
2018-10-07 00:40:45 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const hbs = require('hbs');	// viewer engine for handlebar.js
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public')); // this is a built-in middleware to server static files
+app.use((request, response, next) => {
+	var now = new Date().toString();
+	var log = `${now}: ${request.method} ${request.url}`;
+
+	fs.appendFile('server.log', log + '\n', (err) => {
+		if (err) {
+			console.log('Unable to append to server.log')
+		}
+	});
+	next();
+});	// this is to create your own middleware, the middleware must call next() to mark that middleware is done
+
+app.use((request, response, next) => {
+	response.render('maintainence.hbs');	// because we did not call the next(), so it stops here.
+});
+
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase();
+});
+
+app.get('/', (request, response) => {
+	// response.send('<h1>Hello Express</h1>');
+	// response.send({
+	// 	name: 'Andrew'
+	// });
+	response.render('home.hbs', {
+		pageTitle: 'Welcome',
+		welcomeMsg: 'Welcome to here'
+	})
+});
+
+app.get('/about', (request, response) => {
+	// response.send('About page');
+	response.render('about.hbs', {
+		pageTitle: 'About Page'
+	});
+});
+
+app.get('/bad', (request, response) => {
+	response.send({
+		errorMessage: 'Bad request'
+	});
+});
+
+app.listen(7000, () => {
+	console.log('Server is up on port 7000');
+});
